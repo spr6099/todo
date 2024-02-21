f3b4e26361181116f50d7bbacd3229d04ad67fa9
sandeep <sandeep.pr3@gmail.com>
2024-02-21 19:41:42 +0530

    todo

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017";
+// const assert = require("assert")
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index' );
+});
+
+router.post('/',function(req,res){
+ res.send("connected")
+ MongoClient.connect(url,function(err,client,next){
+  if(err) throw err;
+  // assert.equal(null,err);
+  const db = client.db("tododb");
+  db.collection("notes")
+  console.log("connected succesfully to the server");
+
+  client.close();
+ })
+})
+
+module.exports = router;
