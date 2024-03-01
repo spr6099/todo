a57388347a1654a5bf097fdaf115e9f01394c18a
sandeep <sandeep.pr3@gmail.com>
2024-03-01 18:56:54 +0530

    update complete

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var mongodb = require("mongodb")
+var mongodb = require("mongodb");
 
 var database = require("../database");
 
@@ -12,7 +12,7 @@ router.get("/", function (req, res, next) {
       .find({})
       .toArray()
       .then((result) => {
-        console.log(result);
+        // console.log(result);
         res.render("index", { result });
       });
   });
@@ -28,21 +28,55 @@ router.post("/", (req, res) => {
       .insertOne(datas)
       .then((result) => {
         console.log("created");
-        console.log(result);
+        // console.log(result);
       });
   });
   res.redirect("/");
 });
 
-router.get("/:id",(req,res)=>{
-  let id=req.params.id;
-  database.then((dbase)=>{
-    dbase.collection("userdata").deleteOne({_id:new mongodb.ObjectId(id)}).then((result)=>{
-      console.log(result);
-      res.redirect("/")
-    })
-  })
+router.get("/delete/:id", (req, res) => {
+  let id = req.params.id;
+  database.then((dbase) => {
+    dbase
+      .collection("userdata")
+      .deleteOne({ _id: new mongodb.ObjectId(id) })
+      .then((result) => {
+        // console.log(result);
+        res.redirect("/");
+      });
+  });
+});
+ 
+router.get("/find/:id", (req, res) => {
+  let id = req.params.id;
+  database.then((dbase) => {
+    dbase
+      .collection("userdata")
+      .findOne({ _id: new mongodb.ObjectId(id) })
+      .then((findResult) => {
+        console.log(findResult);
+        res.render("update",{findResult});
+      });
+  });
+});
 
-})
+
+router.post("/find/:id", (req, res) => {
+  var id=req.params.id;
+  var datas = {
+    todo: req.body.todos,
+  };
+  database.then((dbase) => {
+    dbase
+      .collection("userdata")
+      .updateOne({ _id: new mongodb.ObjectId(id) },{$set:datas})
+      .then((result) => {
+        console.log("created");
+        // console.log(result);
+
+      });
+  });
+  res.redirect("/");
+});
 
 module.exports = router;
